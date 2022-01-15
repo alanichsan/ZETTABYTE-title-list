a588bd92e36c13cd1277a747c3fc3631bfa45263
alanichsan <alanxrpl2018smkrus@gmail.com>
2022-01-15 20:17:45 +0700

    fix maximum budget

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,6 +1,5 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { ListBookComponent } from './list-book/list-book.component';
 import { HomeComponent } from './home/home.component';
 
 const routes: Routes = [
@@ -8,10 +7,6 @@ const routes: Routes = [
     path : 'home',
     component : HomeComponent,
   },
-  {
-    path : 'list-book',
-    component : ListBookComponent,
-  }
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,7 +4,6 @@ import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { ListBookComponent } from './list-book/list-book.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { HomeComponent } from './home/home.component';
@@ -12,7 +11,6 @@ import { HomeComponent } from './home/home.component';
 @NgModule({
   declarations: [
     AppComponent,
-    ListBookComponent,
     NavbarComponent,
     HomeComponent,
   ],
